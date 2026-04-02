cab729f424a6a6d101b3f10ca9ac2644dcfbde2d
tesskhouston <tess4504@gmail.com>
2026-04-02 16:34:16 -0600

    Updated code
    Implemented second diode

--- adc-code.py
+++ adc-code.py
@@ -7,13 +7,16 @@ import time
 import pandas as pd
 import datetime
 
-drops = 0
-adc_data = []
-volts = []
-drop_time = []
+drops1 = 0
+drops2 = 0
+adc_data1 = []
+adc_data2 = []
+volts1 = []
+volts2 = []
+drop_time1 = []
+drop_time2=[]
 dates = []
-index = 0
-last_drop = 0
+
 
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -21,36 +24,54 @@ cs = digitalio.DigitalInOut(board.D5)
 
 mcp = MCP.MCP3008(spi, cs)
 
-chan = AnalogIn(mcp, MCP.P0)
+#yellow wire
+chan1 = AnalogIn(mcp, MCP.P0)
 
-def noise_filter(data):
-    global drops
-    global last_drop
+#green wire
+chan2 = AnalogIn(mcp, MCP.P1)
+
+
+def noise_filter(data, channel):
     #approximate normal (unimpeded) values are around 61000
-    if data<55000 and adc_data[len(adc_data)-2]>50000:
-        drops+=1
-        last_drop = index
-        drop_time.append(1)
+    if data<55000 and channel[len(channel)-2]>55000:
+        return 1
     else:
-        drop_time.append(0)
+        return 0
         
-    
 
 def collect():
-    adc_data.append(chan.value)
-    volts.append(chan.voltage)
-    noise_filter(chan.value)
-    index = len(adc_data)
+    global drops1
+    global drops2
+    
+    adc_data1.append(chan1.value)
+    volts1.append(chan1.voltage)
+    if noise_filter(chan1.value, adc_data1) == 1:
+        drops1+=1
+        drop_time1.append(1)
+    else:
+        drop_time1.append(0)
+        
+    adc_data2.append(chan2.value)
+    volts2.append(chan2.voltage)
+    if noise_filter(chan2.value, adc_data2) == 1:
+        drops2+=1
+        drop_time2.append(1)
+    else:
+        drop_time2.append(0)
+        
     dates.append(datetime.datetime.now().replace(microsecond=0))
-    print("Raw ADC Value: ", chan.value)
-    print("ADC Voltage: " + str(chan.voltage) + "V")
-    print(drops)
+    print("Raw ADC Value Channel 1: ", chan1.value)
+    print("ADC Voltage Channel 1: " + str(chan1.voltage) + "V")
+    print(drops1)
+    print("Raw ADC Value Channel 2: ", chan2.value)
+    print("ADC Voltage Channel 2: " + str(chan2.voltage) + "V")
+    print(drops2)
     print("--------------------------")
     time.sleep(.1)
 
 while True:
     collect()
 
-    df= pd.DataFrame(list(zip(dates, volts, adc_data, drop_time)), columns = ["Datetime", "ADC Volts", "ADC Values", "Drops"])
+    df= pd.DataFrame(list(zip(dates, volts1, volts2, adc_data1, adc_data2, drop_time1, drop_time2)), columns = ["Datetime", "ADC Volts Ch1","ADC Volts Ch2", "ADC Values Ch1", "ADC Values Ch2", "Drops Ch1", "Drops Ch2"])
 
     df.to_csv('precipitation.csv', index=False)
