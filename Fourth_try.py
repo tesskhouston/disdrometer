d0be26b4cd91a549bf2fa85ec7ec1ec336621e22
tesskhouston <tess4504@gmail.com>
2026-07-31 18:23:02 -0600

    Added all diodes and filter

---
+++ Fourth_try.py
@@ -0,0 +1,152 @@
+import Graph_in_py
+import adc_code
+from datetime import datetime
+from matplotlib import pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from scipy.ndimage import gaussian_filter1d
+
+
+while True:
+    start_time=datetime.now().timestamp()
+    while datetime.now().timestamp()-start_time<adc_code.sampling_time:
+        adc_code.collect()
+    adc_code.noise_filter(adc_code.adc_data1, adc_code.volts1, adc_code.drops1, adc_code.drop1_ct, adc_code.drop_time1)
+    adc_code.noise_filter(adc_code.adc_data2, adc_code.volts2, adc_code.drops2, adc_code.drop2_ct, adc_code.drop_time2)
+    adc_code.noise_filter(adc_code.adc_data3, adc_code.volts3, adc_code.drops3, adc_code.drop3_ct, adc_code.drop_time3)
+    adc_code.noise_filter(adc_code.adc_data4, adc_code.volts4, adc_code.drops4, adc_code.drop4_ct, adc_code.drop_time4)
+    adc_code.noise_filter(adc_code.adc_data5, adc_code.volts5, adc_code.drops5, adc_code.drop5_ct, adc_code.drop_time5)
+    adc_code.noise_filter(adc_code.adc_data6, adc_code.volts6, adc_code.drops6, adc_code.drop6_ct, adc_code.drop_time6)
+    adc_code.noise_filter(adc_code.adc_data7, adc_code.volts7, adc_code.drops7, adc_code.drop7_ct, adc_code.drop_time7)
+    adc_code.noise_filter(adc_code.adc_data8, adc_code.volts8, adc_code.drops8, adc_code.drop8_ct, adc_code.drop_time8)
+    adc_code.noise_filter(adc_code.adc_data1_1, adc_code.volts1_1, adc_code.drops1_1, adc_code.drop1_1_ct, adc_code.drop_time1_1)
+    adc_code.noise_filter(adc_code.adc_data1_2, adc_code.volts1_2, adc_code.drops1_2, adc_code.drop1_2_ct, adc_code.drop_time1_2)
+    adc_code.noise_filter(adc_code.adc_data1_3, adc_code.volts1_3, adc_code.drops1_3, adc_code.drop1_3_ct, adc_code.drop_time1_3)
+    adc_code.noise_filter(adc_code.adc_data1_4, adc_code.volts1_4, adc_code.drops1_4, adc_code.drop1_4_ct, adc_code.drop_time1_4)
+    adc_code.noise_filter(adc_code.adc_data1_5, adc_code.volts1_5, adc_code.drops1_5, adc_code.drop1_5_ct, adc_code.drop_time1_5)
+    adc_code.noise_filter(adc_code.adc_data1_6, adc_code.volts1_6, adc_code.drops1_6, adc_code.drop1_6_ct, adc_code.drop_time1_6)
+    adc_code.noise_filter(adc_code.adc_data1_7, adc_code.volts1_7, adc_code.drops1_7, adc_code.drop1_7_ct, adc_code.drop_time1_7)
+    adc_code.noise_filter(adc_code.adc_data1_8, adc_code.volts1_8, adc_code.drops1_8, adc_code.drop1_8_ct, adc_code.drop_time1_8)
+
+    adc_code.backfill_drops()
+    plt.clf()
+    plt.close('all')
+    adc_code.total_drops_per_sec()
+    adc_code.backfill_drops()
+    adc_code.dataframe()
+    Graph_in_py.update_csv()
+
+    #For most accurate results, do not make a plot during code running. However,
+    #it is an option if live results are needed or user preference favors live
+
+    #This string is code that makes a plot with three subplots, one for each
+    #category. plt.pause needs to be at most 1 second so that code has time to
+    #plot before continuing to take measurements (if doesn't work, up time)
+    #Note: this setup only displays data for one sensor. Additional sensors will
+    #need to be added
+    """fig, axs = plt.subplots(3)
+    axs[0].plot("Datetime", "ADC Values Ch1", data=Graph_in_py.update_csv())
+    axs[0].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[1].plot("Datetime", "Drop Occurences Ch1", data=Graph_in_py.update_csv())
+    axs[1].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[2].plot("Datetime", "Cumulative Drops Ch1", data=Graph_in_py.update_csv())
+    axs[2].xaxis.set_major_locator(MaxNLocator(nbins=5))"""
+
+    #This line should be commented out if using the other graph setup or not graphing live.
+    #Only graphs the cumulative raindrops over time, leaving out other two
+    #If using this version, graph should need at most 0.15 seconds to process
+    #as graphs continue for longer, wait time increases
+    fig, axs = plt.subplots(17)
+    axs[0].plot("Datetime", "Cumulative Drops Ch1", data=Graph_in_py.update_csv())
+    axs[0].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[1].plot("Datetime", "Cumulative Drops Ch2", data=Graph_in_py.update_csv())
+    axs[1].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[2].plot("Datetime", "Cumulative Drops Ch3", data=Graph_in_py.update_csv())
+    axs[2].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[3].plot("Datetime", "Cumulative Drops Ch4", data=Graph_in_py.update_csv())
+    axs[3].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[4].plot("Datetime", "Cumulative Drops Ch5", data=Graph_in_py.update_csv())
+    axs[4].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[5].plot("Datetime", "Cumulative Drops Ch6", data=Graph_in_py.update_csv())
+    axs[5].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[6].plot("Datetime", "Cumulative Drops Ch7", data=Graph_in_py.update_csv())
+    axs[6].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[7].plot("Datetime", "Cumulative Drops Ch8", data=Graph_in_py.update_csv())
+    axs[7].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[8].plot("Datetime", "Cumulative Drops Ch1_1", data=Graph_in_py.update_csv())
+    axs[8].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[9].plot("Datetime", "Cumulative Drops Ch1_2", data=Graph_in_py.update_csv())
+    axs[9].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[10].plot("Datetime", "Cumulative Drops Ch1_3", data=Graph_in_py.update_csv())
+    axs[10].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[11].plot("Datetime", "Cumulative Drops Ch1_4", data=Graph_in_py.update_csv())
+    axs[11].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[12].plot("Datetime", "Cumulative Drops Ch1_5", data=Graph_in_py.update_csv())
+    axs[12].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[13].plot("Datetime", "Cumulative Drops Ch1_6", data=Graph_in_py.update_csv())
+    axs[13].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[14].plot("Datetime", "Cumulative Drops Ch1_7", data=Graph_in_py.update_csv())
+    axs[14].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[15].plot("Datetime", "Cumulative Drops Ch1_8", data=Graph_in_py.update_csv())
+    axs[15].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    """axs[0].plot("Datetime", "ADC Values Ch1", data=Graph_in_py.update_csv())
+    axs[0].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[1].plot("Datetime", "ADC Values Ch2", data=Graph_in_py.update_csv())
+    axs[1].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[2].plot("Datetime", "ADC Values Ch3", data=Graph_in_py.update_csv())
+    axs[2].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[3].plot("Datetime", "ADC Values Ch4", data=Graph_in_py.update_csv())
+    axs[3].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[4].plot("Datetime", "ADC Values Ch5", data=Graph_in_py.update_csv())
+    axs[4].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[5].plot("Datetime", "ADC Values Ch6", data=Graph_in_py.update_csv())
+    axs[5].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[6].plot("Datetime", "ADC Values Ch7", data=Graph_in_py.update_csv())
+    axs[6].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[7].plot("Datetime", "ADC Values Ch8", data=Graph_in_py.update_csv())
+    axs[7].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[8].plot("Datetime", "ADC Values Ch1_1", data=Graph_in_py.update_csv())
+    axs[8].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[9].plot("Datetime", "ADC Values Ch1_2", data=Graph_in_py.update_csv())
+    axs[9].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[10].plot("Datetime", "ADC Values Ch1_3", data=Graph_in_py.update_csv())
+    axs[10].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[11].plot("Datetime", "ADC Values Ch1_4", data=Graph_in_py.update_csv())
+    axs[11].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[12].plot("Datetime", "ADC Values Ch1_5", data=Graph_in_py.update_csv())
+    axs[12].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[13].plot("Datetime", "ADC Values Ch1_6", data=Graph_in_py.update_csv())
+    axs[13].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[14].plot("Datetime", "ADC Values Ch1_7", data=Graph_in_py.update_csv())
+    axs[14].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[15].plot("Datetime", "ADC Values Ch1_8", data=Graph_in_py.update_csv())
+    axs[15].xaxis.set_major_locator(MaxNLocator(nbins=5))"""
+    
+    axs[16].plot("Datetime", "Total Drops per Second", data=Graph_in_py.update_csv())
+    axs[16].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    
+    plt.gcf().autofmt_xdate()
+    #plt.tight_layout()
+
+    
+    """fig, axs = plt.subplots(3)
+    axs[0].plot(adc_code.dates, adc_code.volts1, label="Noise")
+    axs[0].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[1].plot(adc_code.dates, adc_code.adc_data1, label="Noise")
+    axs[1].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    axs[2].plot(adc_code.dates, adc_code.drop1_ct, label="Noise")
+    axs[2].xaxis.set_major_locator(MaxNLocator(nbins=5))
+    
+    plt.legend()"""
+
+    plt.show()
+    #change seconds in pause depending on version of graph chosen
+    """plt.pause(1)
+    print(adc_code.total_drops_per_sec())"""
+    print("That's it!")
+    adc_code.increase_i()
+    
+    
+    
+        
+    
+    
---
+++ adc_code.py
@@ -0,0 +1,345 @@
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+import time
+import pandas as pd
+import datetime
+import gpiozero
+import numpy as np
+
+
+drops1 = 0
+drops2 = 0
+drop1_ct = []
+drop2_ct = []
+adc_data1 = []
+adc_data2 = []
+volts1 = []
+volts2 = []
+drop_time1 = []
+drop_time2=[]
+drops3 = 0
+drops4 = 0
+drop3_ct = []
+drop4_ct = []
+adc_data3 = []
+adc_data4 = []
+volts3 = []
+volts4 = []
+drop_time3 = []
+drop_time4=[]
+drops5 = 0
+drops6 = 0
+drop5_ct = []
+drop6_ct = []
+adc_data5 = []
+adc_data6 = []
+volts5 = []
+volts6 = []
+drop_time5 = []
+drop_time6=[]
+drops7 = 0
+drops8 = 0
+drop7_ct = []
+drop8_ct = []
+adc_data7 = []
+adc_data8 = []
+volts7 = []
+volts8 = []
+drop_time7 = []
+drop_time8=[]
+
+drops1_1 = 0
+drops1_2 = 0
+drop1_1_ct = []
+drop1_2_ct = []
+adc_data1_1 = []
+adc_data1_2 = []
+volts1_1 = []
+volts1_2 = []
+drop_time1_1 = []
+drop_time1_2=[]
+drops1_3 = 0
+drops1_4 = 0
+drop1_3_ct = []
+drop1_4_ct = []
+adc_data1_3 = []
+adc_data1_4 = []
+volts1_3 = []
+volts1_4 = []
+drop_time1_3 = []
+drop_time1_4=[]
+drops1_5 = 0
+drops1_6 = 0
+drop1_5_ct = []
+drop1_6_ct = []
+adc_data1_5 = []
+adc_data1_6 = []
+volts1_5 = []
+volts1_6 = []
+drop_time1_5 = []
+drop_time1_6=[]
+drops1_7 = 0
+drops1_8 = 0
+drop1_7_ct = []
+drop1_8_ct = []
+adc_data1_7 = []
+adc_data1_8 = []
+volts1_7 = []
+volts1_8 = []
+drop_time1_7 = []
+drop_time1_8=[]
+
+dates = []
+total_drops_sec = []
+
+sampling_time = 10
+prev_total_drops_sec = 0
+
+i=0
+sample_id = []
+
+
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+cs = digitalio.DigitalInOut(board.D5)
+
+cs1 = digitalio.DigitalInOut(board.D6)
+
+mcp = MCP.MCP3008(spi, cs)
+mcp1 = MCP.MCP3008(spi, cs1)
+
+#yellow wire 1
+chan1 = AnalogIn(mcp, MCP.P0)
+
+#green wire 1
+chan2 = AnalogIn(mcp, MCP.P1)
+
+#yellow wire 2
+chan3 = AnalogIn(mcp, MCP.P2)
+
+#green wire 2
+chan4 = AnalogIn(mcp, MCP.P3)
+
+#yellow wire 3
+chan5 = AnalogIn(mcp, MCP.P4)
+
+#green wire 3
+chan6 = AnalogIn(mcp, MCP.P5)
+
+#yellow wire 4
+chan7 = AnalogIn(mcp, MCP.P6)
+
+#green wire 4
+chan8 = AnalogIn(mcp, MCP.P7)
+
+chan1_1 = AnalogIn(mcp1, MCP.P0)
+
+chan1_2 = AnalogIn(mcp1, MCP.P1)
+
+chan1_3 = AnalogIn(mcp1, MCP.P2)
+
+chan1_4 = AnalogIn(mcp1, MCP.P3)
+
+chan1_5 = AnalogIn(mcp1, MCP.P4)
+
+chan1_6 = AnalogIn(mcp1, MCP.P5)
+
+chan1_7 = AnalogIn(mcp1, MCP.P6)
+
+chan1_8 = AnalogIn(mcp1, MCP.P7)
+
+
+def noise_filter(va_data, vo_data, dropnum, dropdata, timedata):
+    """#approximate normal (unimpeded) values are around 61000
+    if data<45000 and channel[len(channel)-2]>45000:
+        return 1
+    else:
+        return 0"""
+    global i
+    start_ind = sample_id.index(i)
+    end_ind = len(sample_id)-1
+    sample_vaarray=[]
+    sample_voarray=[]
+    j=start_ind
+    while end_ind>=j:
+        sample_vaarray.append(va_data[j])
+        sample_voarray.append(vo_data[j])
+        j+=1
+    Q1Va=np.percentile(sample_vaarray, 25, method='midpoint')
+    Q1Vo=np.percentile(sample_voarray, 25, method='midpoint')
+    Q3Va=np.percentile(sample_vaarray, 75, method='midpoint')
+    Q3Vo=np.percentile(sample_voarray, 75, method='midpoint')
+    VaIQR = Q3Va-Q1Va
+    VoIQR=Q3Vo-Q1Vo
+    lower_va=Q1Va-9*VaIQR
+    lower_vo=Q1Vo-2*VoIQR
+    #print(lower_va)
+    j=start_ind
+    while end_ind>=j:
+        if va_data[j]<=lower_va and va_data[j-1]>lower_va:
+            dropnum+=1
+            timedata.append(1)
+        else:
+            timedata.append(0)
+        dropdata.append(dropnum)
+        
+        j+=1
+        
+        
+
+def collect():
+
+    global drops1
+    global drops2
+    global drops3
+    global drops4
+    global drops5
+    global drops6
+    global drops7
+    global drops8
+    global drops1_1
+    global drops1_2
+    global drops1_3
+    global drops1_4
+    global drops1_5
+    global drops1_6
+    global drops1_7
+    global drops1_8
+    global prev_total_drops_sec
+    
+
+    adc_data1.append(chan1.value)
+    volts1.append(chan1.voltage)
+    
+    
+    adc_data2.append(chan2.value)
+    volts2.append(chan2.voltage)
+
+
+    adc_data3.append(chan3.value)
+    volts3.append(chan3.voltage)
+
+    
+    adc_data4.append(chan4.value)
+    volts4.append(chan4.voltage)
+
+
+    adc_data5.append(chan5.value)
+    volts5.append(chan5.voltage)
+
+    
+    adc_data6.append(chan6.value)
+    volts6.append(chan6.voltage)
+
+
+
+    adc_data7.append(chan7.value)
+    volts7.append(chan7.voltage)
+
+    
+    adc_data8.append(chan8.value)
+    volts8.append(chan8.voltage)
+
+
+    adc_data1_1.append(chan1_1.value)
+    volts1_1.append(chan1_1.voltage)
+
+
+    adc_data1_2.append(chan1_2.value)
+    volts1_2.append(chan1_2.voltage)
+
+
+    adc_data1_3.append(chan1_3.value)
+    volts1_3.append(chan1_3.voltage)
+
+
+    adc_data1_4.append(chan1_4.value)
+    volts1_4.append(chan1_4.voltage)
+    
+
+    adc_data1_5.append(chan1_5.value)
+    volts1_5.append(chan1_5.voltage)
+
+    adc_data1_6.append(chan1_6.value)
+    volts1_6.append(chan1_6.voltage)
+
+
+    adc_data1_7.append(chan1_7.value)
+    volts1_7.append(chan1_7.voltage)
+    
+
+    adc_data1_8.append(chan1_8.value)
+    volts1_8.append(chan1_8.voltage)
+
+    
+    dates.append(datetime.datetime.now())
+    total_drops_sec.append(prev_total_drops_sec)
+    sample_id.append(i)
+    
+
+    
+                 #.replace(microsecond=0))
+    
+    
+    """print("Raw ADC Value Channel 1: ", drops1)
+    print("Raw ADC Value Channel 2: ", drops2)
+    print("Raw ADC Value Channel 3: ", drops3)
+    print("Raw ADC Value Channel 4: ", drops4)
+    print("Raw ADC Value Channel 5: ", drops5)
+    print("Raw ADC Value Channel 6: ", drops6)
+    print("Raw ADC Value Channel 7: ", drops7)
+    print("Raw ADC Value Channel 8: ", drops8)
+    print("Raw ADC Value Channel 1_1: ", drops1_1)
+    print("Raw ADC Value Channel 1_2: ", drops1_2)
+    print("Raw ADC Value Channel 1_3: ", drops1_3)
+    print("Raw ADC Value Channel 1_4: ", drops1_4)
+    print("Raw ADC Value Channel 1_5: ", drops1_5)
+    print("Raw ADC Value Channel 1_6: ", drops1_6)
+    print("Raw ADC Value Channel 1_7: ", drops1_7)
+    print("Raw ADC Value Channel 1_8: ", drops1_8)"""
+    
+    """print("ADC Voltage Channel 1: " + str(chan1.voltage) + "V")
+    print(drops1)
+    print("Raw ADC Value Channel 2: ", chan2.value)
+    print("ADC Voltage Channel 2: " + str(chan2.voltage) + "V")
+    print(drops2)
+    print("--------------------------")"""
+    
+def increase_i():
+    global i
+    i+=1
+    
+def backfill_drops():
+    
+    if len(total_drops_sec) > 0:
+        start = sample_id.index(i)
+        end = len(dates)-1
+        k=start
+        while k<=end:
+            total_drops_sec[k]=prev_total_drops_sec
+            k+=1
+    else:
+        total_drops_sec.append(prev_total_drops_sec)
+    
+
+
+def drops_per_sec(channel):
+    #"channel" input for this function is drop_ct list for chosen channel
+    accumulation = 0
+    start = sample_id.index(i)
+    end = len(dates)-1
+    accumulation = channel[end]-channel[start]
+    return float(float(accumulation)/float(sampling_time))
+
+def total_drops_per_sec():
+    global prev_total_drops_sec
+    prev_total_drops_sec = drops_per_sec(drop1_ct) + drops_per_sec(drop2_ct) + drops_per_sec(drop3_ct)+drops_per_sec(drop4_ct)+drops_per_sec(drop5_ct)+drops_per_sec(drop6_ct)+drops_per_sec(drop7_ct)+drops_per_sec(drop8_ct)+drops_per_sec(drop1_1_ct)+drops_per_sec(drop1_2_ct)+drops_per_sec(drop1_3_ct)+drops_per_sec(drop1_4_ct)+drops_per_sec(drop1_5_ct)+drops_per_sec(drop1_6_ct)+drops_per_sec(drop1_7_ct)+drops_per_sec(drop1_8_ct)
+    return prev_total_drops_sec
+
+def dataframe():
+    df= pd.DataFrame(list(zip(dates, sample_id, volts1, volts2, volts3, volts4, volts5, volts6, volts7, volts8, volts1_1, volts1_2, volts1_3, volts1_4, volts1_5, volts1_6, volts1_7, volts1_8, adc_data1, adc_data2, adc_data3, adc_data4, adc_data5, adc_data6, adc_data7, adc_data8, adc_data1_1, adc_data1_2, adc_data1_3, adc_data1_4, adc_data1_5, adc_data1_6, adc_data1_7, adc_data1_8, drop_time1, drop_time2, drop_time3, drop_time4, drop_time5, drop_time6, drop_time7, drop_time8, drop_time1_1, drop_time1_2, drop_time1_3, drop_time1_4, drop_time1_5, drop_time1_6, drop_time1_7, drop_time1_8, drop1_ct, drop2_ct, drop3_ct, drop4_ct, drop5_ct, drop6_ct, drop7_ct, drop8_ct, drop1_1_ct, drop1_2_ct, drop1_3_ct, drop1_4_ct, drop1_5_ct, drop1_6_ct, drop1_7_ct, drop1_8_ct, total_drops_sec)),
+                     columns = ["Datetime", "Sample ID", "ADC Volts Ch1","ADC Volts Ch2", "ADC Volts Ch3", "ADC Volts Ch4", "ADC Volts Ch5", "ADC Volts Ch6", "ADC Volts Ch7", "ADC Volts Ch8", "ADC Volts Ch1_1", "ADC Volts Ch1_2", "ADC Volts Ch1_3", "ADC Volts Ch1_4", "ADC Volts Ch1_5", "ADC Volts Ch1_6", "ADC Volts Ch1_7", "ADC Volts Ch1_8", "ADC Values Ch1", "ADC Values Ch2", "ADC Values Ch3", "ADC Values Ch4", "ADC Values Ch5", "ADC Values Ch6", "ADC Values Ch7", "ADC Values Ch8", "ADC Values Ch1_1", "ADC Values Ch1_2", "ADC Values Ch1_3", "ADC Values Ch1_4", "ADC Values Ch1_5", "ADC Values Ch1_6", "ADC Values Ch1_7", "ADC Values Ch1_8", "Drop Occurences Ch1", "Drop Occurences Ch2", "Drop Occurences Ch3", "Drop Occurences Ch4", "Drop Occurences Ch5", "Drop Occurences Ch6", "Drop Occurences Ch7", "Drop Occurences Ch8", "Drop Occurences Ch1_1", "Drop Occurences Ch1_2", "Drop Occurences Ch1_3", "Drop Occurences Ch1_4", "Drop Occurences Ch1_5", "Drop Occurences Ch1_6", "Drop Occurences Ch1_7", "Drop Occurences Ch1_8", "Cumulative Drops Ch1", "Cumulative Drops Ch2", "Cumulative Drops Ch3", "Cumulative Drops Ch4", "Cumulative Drops Ch5", "Cumulative Drops Ch6", "Cumulative Drops Ch7", "Cumulative Drops Ch8", "Cumulative Drops Ch1_1", "Cumulative Drops Ch1_2", "Cumulative Drops Ch1_3", "Cumulative Drops Ch1_4", "Cumulative Drops Ch1_5", "Cumulative Drops Ch1_6", "Cumulative Drops Ch1_7", "Cumulative Drops Ch1_8", "Total Drops per Second"])
+    df.to_csv('precipitation.csv', index=False)
